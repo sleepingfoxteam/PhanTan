1ba60173729a7d8ee48a5315cb06db89c83f3a9a
tranphathung <hungtp.personal@gmail.com>
2019-06-08 16:45:33 +0700

    chinh _layout_khachhang.cshtml

---
+++ PhanTan/PhanTan_ASP/Controllers/DichVusController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhanTan_ASP.Controllers
+{
+    public class DichVusController : Controller
+    {
+        // GET: DichVus
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TaiKhoanThanhToan
+        public ActionResult TaiKhoanThanhToan()
+        {
+            return View();
+        }
+
+        //GET: TaiKhoanTietKiem
+        public ActionResult TaiKhoanTietKiem()
+        {
+            return View();
+        }
+
+        public ActionResult TheThanhToan()
+        {
+            return View();
+        }
+
+        public ActionResult InternetBanking()
+        {
+            return View();
+        }
+    }
+}
