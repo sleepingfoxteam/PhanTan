f8952462cbd149cc990b3156e4c837bcfa8e3b2a
tranphathung <hungtp.personal@gmail.com>
2019-06-08 15:42:06 +0700

    them controller user, login, innerlogin

---
+++ PhanTan/PhanTan_ASP/Controllers/InnerLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhanTan_ASP.Controllers
+{
+    public class InnerLoginController : Controller
+    {
+        // GET: InnerLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
