e7f55c13954a51c94ac6392229ea2885aa66a024
tranphathung <hungtp.personal@gmail.com>
2019-06-15 12:15:52 +0700

    xong dang nhap buoc 1

--- TUIBANK_WEBAPP/Controllers/LoginController.cs
+++ TUIBANK_WEBAPP/Controllers/LoginController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Linq;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
+using TUIBANK_WEBAPP.Models;
 
 namespace TUIBANK_WEBAPP.Controllers
 {
@@ -17,6 +22,52 @@ namespace TUIBANK_WEBAPP.Controllers
         public ActionResult DangXuat()
         {
             return View("Index", "~/Views/Shared/_empty_layout.cshtml");
+        }
+
+        [HttpPost]
+        public dynamic Login()
+        {
+            string username = Request["username"];
+            string password = Request["password"];
+            string branch = Request["branch"];
+            branch = branch.Equals("BRHCM")==true ? "CN1" : "CN2";
+            string CONNECTION_STRING = "metadata=res://*/Models.TUIBANK.csdl|res://*/Models.TUIBANK.ssdl|res://*/Models.TUIBANK.msl;provider=System.Data.SqlClient;provider connection string=\"data source=DESKTOP-4AOI1UG\\"+branch+";initial catalog=TUIBANK;user id="+username+";password="+password+";MultipleActiveResultSets=True;App=EntityFramework\"";
+            string SHORT = "Data Source = DESKTOP-4AOI1UG\\"+branch+"; Initial Catalog = TUIBANK; Persist Security Info = True; User ID ="+username+ "; Password = " + password;
+            SqlConnection conn = new SqlConnection(SHORT);
+            bool loged = false;
+            try
+            {
+                conn.Open();
+                if(conn.State == System.Data.ConnectionState.Open)
+                {
+                    loged = true;
+                }
+                conn.Close();
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            if(loged == true)
+            {
+                try
+                {
+                    Configuration connectionConfiguration = WebConfigurationManager.OpenWebConfiguration("~");
+                    connectionConfiguration.ConnectionStrings.ConnectionStrings["TUIBANKEntities"].ConnectionString = CONNECTION_STRING;
+                    connectionConfiguration.Save(ConfigurationSaveMode.Modified);
+                    ConfigurationManager.RefreshSection("connectionStrings");
+                    // TODO: get role of user
+                    return RedirectToAction("Index", "Home", new { role = "Manager"});
+                }
+                catch(Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
